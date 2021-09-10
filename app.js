338c05715f0a771cac0ca7a4c0358bf726591f97
Joao Prates <joaoprates.az@gmail.com>
2021-09-10 14:36:44 +0100

    Fixed some issues

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const projectName = "project2-helpr";
 const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
+app.locals.gmapsKey = process.env.GOOGLE_MAPS_KEY;
 
 // 👇 Start handling routes here
 const index = require("./routes/index");
