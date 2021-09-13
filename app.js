7b76f6f7b6b7a359ab0a4c7ab833cc68bdf401ff
michaelcdixon <michaelmail7@gmail.com>
2021-09-13 10:12:55 +0100

    Authentication working

--- app.js
+++ app.js
@@ -29,10 +29,8 @@ app.locals.gmapsKey = process.env.GOOGLE_MAPS_KEY;
 // 👇 Start handling routes here
 const index = require("./routes/index");
 app.use("/", index);
-/* const signup = require("./routes/signup");
-app.use("/", index);
-const login = require("./routes/login");
-app.use("/", index); */
+const auth = require("./routes/auth");
+app.use("/", auth);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
