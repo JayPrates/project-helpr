2fd2b0bc70be3b314c13eb4f0a5f6498974c453b
michaelcdixon <michaelmail7@gmail.com>
2021-09-16 10:17:16 +0100

    more styles complete

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,6 @@ const router = require("express").Router();
 const Coordinates = require("../models/Coord.model");
 const User = require("../models/User.model");
 
-// let vOneLs = localStorage.getItem("vOneLocalStorage");
-// let vTwoLs = localStorage.getItem("vTwoLocalStorage");
-// import { latitude } from "../public/js/main.js";
-// import { longitude } from "../public/js/main.js";
-
-// const coordinates = require("../public/js/main");
-// require("../public/js/main.js");
-
 /* GET home page */
 
 router.get("/", async (req, res, next) => {
@@ -24,11 +16,6 @@ router.get("/request", async (req, res) => {
 
 router.post("/request", async (req, res) => {
 	const { title, description, latitude, longitude } = req.body;
-	/* console.log("latitude", latitude);
-	console.log("long", longitude);
-	console.log("title", title);
-	console.log("description", description);
-	console.log('this user', req.session.currentUser); */
 
 	const thisUsa = await User.findById(req.session.currentUser._id);
 	console.log(thisUsa);
@@ -39,12 +26,11 @@ router.post("/request", async (req, res) => {
 		lat: latitude,
 		long: longitude,
 		user: thisUsa.username,
-		userImg: thisUsa.imageUrl
+		userImg: thisUsa.imageUrl,
 	});
 	const allCoords = await Coordinates.find();
 	/* console.log(allCoords); */
 	res.redirect("/");
 });
-
 
 module.exports = router;
